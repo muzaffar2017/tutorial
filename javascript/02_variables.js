6cd2bbe43794b776edf0b64debec6bfd34025171
muzaffar2017 <84658121+muzaffar2017@users.noreply.github.com>
2024-07-23 08:53:10 +0000

    variables added

---
+++ javascript/02_variables.js
@@ -0,0 +1,12 @@
+const AccountId = "FY2024"
+
+let AccountName = "ABC"
+var age = 39
+
+//const AccountId = "FY2023" //const variable cannot be declare multiple time
+
+//console.log(AccountId); // for printing single output
+
+console.table([AccountId, AccountName, age]) // display all variable values
+
+
